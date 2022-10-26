c5d81df458c2c35bd4f3c8387cda2d18bf051dda
Zaczek23347 <92017943+Zaczek23347@users.noreply.github.com>
2022-10-26 13:26:14 +0200

    Update cw3_zad2.py

--- cw3_zad1-4/cw3_zad2.py
+++ cw3_zad1-4/cw3_zad2.py
@@ -2,17 +2,17 @@ from klasy import Coin
 
 
 def main():
-    m1: Coin = Coin(True)
+    m1: Coin = Coin()
     print(m1.show_side())
     m1.throw()
     print(m1.show_side())
 
-    m2: Coin = Coin(False)
+    m2: Coin = Coin()
     print(m2.show_side())
     m2.throw()
     print(m2.show_side())
 
-    m3: Coin = Coin(True)
+    m3: Coin = Coin()
     print(m3.show_side())
     m3.throw()
     print(m3.show_side())
