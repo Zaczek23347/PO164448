fb877a45ead207545cf1aca4967880f6ff840072
Zaczek23347 <92017943+Zaczek23347@users.noreply.github.com>
2022-12-07 15:26:25 +0100

    java

---
+++ cw1_java/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        String a = input.nextLine();
+        System.out.println(a);
+        int x = input.nextInt();
+        System.out.println(x);
+    }
+    }
